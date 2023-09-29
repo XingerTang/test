63099d665ca5f15cf24610b5e4b595d9e34052a9
xinger <s2186420@ed.ac.uk>
2023-09-29 13:54:29 +0100

    Changes

--- src/test/testing.py
+++ src/test/testing.py
@@ -1,66 +1,151 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import operator
 
-test_single_no_recomb = np.array(
-    [0.865, 1.000, 0.884, 0.855, 0.835, 0.697]
-)
+# def read_file(file_path, **kwargs):
+#     """
+#     INPUT:
+#     file_path: str, the path of the file to be read
+#     decimal_place(optional): if provided, round the data with the given
+#     decimal places
+#     OUTPUT:
+#     values: 2d list of str, store the values of the records
+#     """
+#     with open(file_path, "r") as file:
+#         values = [line.strip().split() for line in file]
 
-test_single_estmaf_no_recomb = np.array(
-    [0.863, 0.996, 0.882, 0.854, 0.835, 0.697]
-)
+#     if "decimal_place" in kwargs.keys():
+#         # round the data if data exists
+#         values = [
+#             [line[0]]
+#             + [round(float(data), kwargs["decimal_place"]) for data in line[1:]]
+#             if line
+#             else line
+#             for line in values
+#         ]
+#     else:
+#         # convert data to float for comparison if data exists
+#         values = [
+#             [line[0]] + [float(data) for data in line[1:]] if line else line
+#             for line in values
+#         ]
 
-test_multi_no_recomb = np.array(
-    [0.980, 0.996, 0.882, 0.854, 0.835, 0.697]
-)
+#     return values
 
-test_multi_estmaf_no_recomb = np.array(
-    [0.977, 0.993, 0.941, 0.976, 0.987, 0.989]
-)
 
-test_multi_estmaf_esterrors_no_recomb = np.array(
-    [0.978, 0.994, 0.943, 0.976, 0.986, 0.990]
-)
+# geno_w_missing = read_file("/Users/evie/Lab/Alpha/AlphaPeel/tests/accuracy_tests/baseData/genotypes.txt")
+# geno_true = read_file("/Users/evie/Lab/Alpha/AlphaPeel/tests/accuracy_tests/baseData/trueGenotypes.txt")
 
-test_multi_seq_no_recomb = np.array(
-    [0.907, 0.797, 0.904, 0.976, 0.992, 0.994]
-)
 
-test_multi_estmaf_seq_no_recomb = np.array(
-    [0.951, 0.877, 0.920, 0.979, 0.993, 0.994]
-)
+# geno_accu = [0] * 5
 
-test_multi_estmaf_esterrors_seq_no_recomb = np.array(
-    [0.951, 0.877, 0.920, 0.979, 0.993, 0.994]
-)
 
-test_hybrid_single_no_recomb = np.array(
-    [0.979, 0.995, 0.948, 0.978, 0.987, 0.989]
-)
+# for gen in range(5):
+#     count = 0
+#     for num in range(200):
+#         id = gen * 200 + num
+#         for locus in range(1000):
+#             if geno_w_missing[id][locus + 1] == 9:
+#                 count += 1
 
-test_hybrid_single_seq_no_recomb = np.array(
-    [0.914, 0.801, 0.923, 0.979, 0.992, 0.994]
-)
+#     geno_accu[gen] = 1 - (count / (200 * 1000))
 
-no_recomb_varname = []
-no_recomb = []
 
-# print(no_recomb.shape)
-# print(no_recomb)
+# print(geno_accu)
+
+columns = (
+        "Type",
+        "Population Accu",
+        "Gen1 Accu",
+        "Gen2 Accu",
+        "Gen3 Accu",
+        "Gen4 Accu",
+        "Gen5 Accu",
+    )
+dt = {"names": columns, "formats": ("S53", "f4", "f4", "f4", "f4", "f4", "f4")}
+accu = np.loadtxt("/Users/evie/Lab/Alpha/AlphaPeel/tests/accuracy_tests/accu_report.txt", dtype=dt)
+
+mkr_accu = list(
+        filter(
+            lambda x: (x["Type"].decode("UTF-8").split("$")[1] == "Marker_accuracies"),
+            accu,
+        )
+    )
+
+ind_accu = list(
+        filter(
+            lambda x: (
+                x["Type"].decode("UTF-8").split("$")[1] == "Individual_accuracies"
+            ),
+            accu,
+        )
+    )
+
+sorted_mkr_accu = sorted(
+        mkr_accu, key=operator.itemgetter(*columns[1:]), reverse=True
+    )
+
+sorted_ind_accu = sorted(
+        ind_accu, key=operator.itemgetter(*columns[1:]), reverse=True
+    )
+
+
+fig, ax = plt.subplots(1, 2)
+
+accu_list = [mkr_accu, ind_accu]
+accu_name_list = ["marker accuracy", "individual accuracy"]
+
+for i in range(2):
+    current_accu = accu_list[i]
+    accu_name = accu_name_list[i]
+    for test in current_accu:
+        ax[i].plot([test["Gen1 Accu"],
+                    test["Gen2 Accu"],
+                    test["Gen3 Accu"],
+                    test["Gen4 Accu"],
+                    test["Gen5 Accu"]], "-.", label = test["Type"].decode("UTF-8").split("$")[0][5:])
+        
+    ax[i].set_xticklabels([f"Gen{gen + 1}" for gen in range(5)])
+    ax[i].set_xticks([0, 1, 2, 3, 4])
+    ax[i].set_xlabel("generations")
+    ax[i].set_ylabel("accuracy")
+    ax[i].set_title(accu_name)
+    ax[i].legend()
+
+
+plt.show()
 
 # fig, ax = plt.subplots()
-# ax.bar(no_recomb_varname, no_recomb[:, 0])
+
+# accu_dict = {}
+# test_name_list = []
+# for test in mkr_accu:
+#     test_name_list.append(test["Type"].decode("UTF-8").split("$")[0][5:])
+
+# accu_dict["marker accuracy"] = []
+# for test in mkr_accu:
+#     accu_dict["marker accuracy"].append(test["Population Accu"])
+
+# accu_dict["individual accuracy"] = []
+# for test in ind_accu:
+#     accu_dict["individual accuracy"].append(test["Population Accu"])
+
+# x = np.arange(len(accu_dict["marker accuracy"]))
+# width = 0.4
+# multiplier = 0
+
+# for test_type, accuracies in accu_dict.items():
+#     offset = width * multiplier
+#     rects = ax.bar(x + offset, accuracies, width, label=test_type)
+#     ax.bar_label(rects)
+#     multiplier += 1
+
+# ax.set_ylabel("Accuracy")
+# ax.set_xlabel("Test")
+# ax.set_title("Accuracies for different tests")
+# ax.set_xticks(x + width)
+# ax.set_xticklabels(test_name_list)
+# ax.set_ylim([0.825, 1])
+# ax.legend()
+
 # plt.show()
-
-local_var = locals()
-local_var_key = local_var.keys()
-
-for item in local_var:
-    pass
-
-for vars in local_var_key:
-    if vars[:-10] == "_no_recomb":
-        no_recomb_varname.append(vars)
-        no_recomb.append(local_var[vars])
-
-print(vars)
-print(local_var)
--- tests/run_test.py
+++ tests/run_test.py
@@ -5,5 +5,19 @@ import numpy as np
 import platform
 import sys
 import shutil
+from multiprocessing import pool
 
 
+def read_file_with_rounding(file_path, decimal_place=4):
+
+    with open(file_path, "r") as file:
+        values = [line.strip().split() for line in file]
+
+    values = [[row[0]] + [
+        str(round(float(data), decimal_place)) for data in row[1:]
+    ] for row in values
+    ]
+
+    print(values)
+
+read_file_with_rounding("/Users/evie/Lab/Alpha/AlphaPeel/tests/functional_tests/test_rec/outputs/rec.seqfile.seg", 2)
